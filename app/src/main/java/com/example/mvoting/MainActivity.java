4ea91ab5d1fa1eec2e0e7f2557b41c3a10b059a5
Varun <avputtur@gmail.com>
2020-11-21 21:30:11 +0400

    fingerprint authentication - added

--- app/src/main/java/com/example/mvoting/MainActivity.java
+++ app/src/main/java/com/example/mvoting/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.mvoting;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.hardware.biometrics.BiometricPrompt;
 import android.os.Bundle;
+import android.os.CancellationSignal;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -25,10 +28,12 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnLogin, btnRegister;
+    Button btnLogin, btnRegister, btnFinger;
     TextView txtNic, txtPin;
     DataBaseHelper db = new DataBaseHelper(this);
     @Override
@@ -38,6 +43,38 @@ public class MainActivity extends AppCompatActivity {
 
         txtNic = findViewById(R.id.editTextTextPersonName);
         txtPin = findViewById(R.id.editTextNumberPassword2);
+
+        Executor executor = Executors.newSingleThreadExecutor();
+
+        BiometricPrompt biometricPrompt = new BiometricPrompt.Builder(this)
+                .setTitle("Fingerprint Authentication")
+                .setSubtitle("Please your finger to your phone screen")
+                .setNegativeButton("Cancel", executor, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).build();
+
+        MainActivity mainActivity = this;
+
+        btnFinger = findViewById(R.id.btnFinger);
+        btnFinger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                biometricPrompt.authenticate(new CancellationSignal(), executor, new BiometricPrompt.AuthenticationCallback() {
+                    @Override
+                    public void onAuthenticationSucceeded(BiometricPrompt.AuthenticationResult result) {
+                        mainActivity.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this, "Authenticated", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+            }
+        });
 
         btnRegister = findViewById(R.id.btnRegister);
         btnRegister.setOnClickListener(new View.OnClickListener() {
